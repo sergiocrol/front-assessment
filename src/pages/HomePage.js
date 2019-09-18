af1e2b1b8a6a9b7bed088450ea4d11ddd6c760b0
Sergio Cordero <sergio.crol@gmail.com>
2019-09-18 16:04:08 +0200

    Routes defined and homepage created

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,31 +5,53 @@ import brastlewarkService from '../services/BrastlewarkService';
 
 class HomePage extends Component {
   state = {
-    inhabitants: []
+    inhabitants: [],
+    paginatedInhabitants: [],
+    itemsPerPage: 135,
+    numberOfPages: 1,
+    currentPage: 1
   }
 
   componentDidMount() {
     brastlewarkService.getAllHabitants()
       .then(response => {
         const inhabitants = response.data.Brastlewark;
-        this.setState({
-          inhabitants
-        })
-        console.log(inhabitants);
+        this.pagination(inhabitants, this.state.currentPage);
       })
       .catch(error => {
         console.log(error);
       });
   }
 
+  pagination = (inhabitants, currentPage) => {
+    const { itemsPerPage } = this.state;
+    const numberOfPages = Math.ceil(inhabitants.length / itemsPerPage);
+    const paginatedInhabitants = inhabitants.slice((itemsPerPage * currentPage) - itemsPerPage, itemsPerPage * currentPage);
+    console.log(paginatedInhabitants);
+    this.setState({
+      inhabitants,
+      numberOfPages,
+      paginatedInhabitants,
+      currentPage
+    })
+  }
+
   render() {
-    const { inhabitants } = this.state;
+    // const { inhabitants } = this.state;
+    const { paginatedInhabitants, numberOfPages, currentPage, inhabitants } = this.state;
     return (
       <div>
-        {inhabitants.length === 0 ? <div>Loading... {/*Cool loading animation*/} </div> : (
-          inhabitants.map(inhabitant => {
-            return <Link key={inhabitant.id} to={{ pathname: `/gnomes/${inhabitant.id}`, gnomeInfo: inhabitant }} params={{ gnomeInfo: inhabitant }}><p>{inhabitant.name}</p></Link>;
-          })
+        {paginatedInhabitants.length === 0 ? <div>Loading... {/*Cool loading animation*/} </div> : (
+          <div>
+            {paginatedInhabitants.map(inhabitant => {
+              return <Link key={inhabitant.id}
+                to={{ pathname: `/gnomes/${inhabitant.id}`, gnomeInfo: inhabitant }}
+                params={{ gnomeInfo: inhabitant }}>
+                <p>{inhabitant.name}</p>
+              </Link>;
+            })}
+            <div>{Array.from(Array(numberOfPages), (e, i) => { return <a key={i} onClick={() => this.pagination(inhabitants, i + 1)}>{i + 1}</a> })}</div>
+          </div>
         )}
       </div >
     );
